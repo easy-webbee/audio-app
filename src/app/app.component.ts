1c1adec2bf3fb2346165dfcaab6ace5c18935356
loc <lluu3790@gmail.com>
2025-10-05 17:28:55 -0500

    test me agian

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
-import { MegaPlayerService } from './mega-player.service';
 import { NgIf } from '@angular/common';
+import { environment } from '../environments/environment.development';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,7 +15,6 @@ export class AppComponent {
   videoUrl?: any;
   constructor(
     private swUpdate: SwUpdate,
-    private mega: MegaPlayerService
   ) {
     this.swUpdate.versionUpdates.subscribe((event) => {
       if (confirm('New version available. Load new version?')) {
@@ -23,14 +22,13 @@ export class AppComponent {
       }
     });
   }
-  async ngOnInit() {
-    console.log(132)
-    this.videoUrl = await this.mega.getVideoBlobUrl(
-      'https://mega.nz/file/ZbpD3LjC#-6YA65wD8Ez5qv7MTFoJ8A99146vl3h1v5BvcylNZFs'
+
+  ngOnInit() {
+    const megaFileUrl = encodeURIComponent(
+      'https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs'
     );
-    console.log(132)
-    console.log(this.videoUrl)
-    console.log('Blob size in bytes:', this.videoUrl.size);
-console.log('Blob size in MB:', (this.videoUrl.size / (1024 * 1024)).toFixed(2), 'MB');
+
+    // Your backend endpoint
+    this.videoUrl = `${environment.keyobUrl}mega/stream?url=${megaFileUrl}`;
   }
 }
