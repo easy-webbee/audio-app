4dd59b049e698cfe463fbb57b501e8f0fe02f332
loc <lluu3790@gmail.com>
2025-10-06 18:31:12 -0500

    next to fix some click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { environment } from '../environments/environment.development';
-
+import { signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,31 +12,19 @@ import { environment } from '../environments/environment.development';
 })
 export class AppComponent {
   title = 'audio-app';
-  videoUrl?: any;
-  constructor(
-
-  ) {
-    // this.swUpdate.versionUpdates.subscribe((event) => {
-    //   if (confirm('New version available. Load new version?')) {
-    //     window.location.reload();
-    //   }
-    // });
-  }
+  constructor(  ) {}
 
   @ViewChild('audio', { static: true }) audioRef!: ElementRef<HTMLAudioElement>;
 
   ngOnInit() {
-    // const megaFileUrl = encodeURIComponent('https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs');
-    // this.videoUrl = `${environment.keyobUrl}mega/stream?url=${megaFileUrl}`;
-    // this.videoUrl = `${environment.keyobUrl}mega/stream?url=${encodeURIComponent('https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs')}`;
   }
 
-  playAudio() {
-    this.audioRef.nativeElement.src = this.videoUrl;
-    this.audioRef.nativeElement.play();
-  }
-  expanded = false;
-  activeIndex: number | null = null;
+  // playAudio() {
+  //   this.audioRef.nativeElement.src = this.videoUrl;
+  //   this.audioRef.nativeElement.play();
+  // }
+  activeIndex: { [bookIndex: number]: number | null } = {};
+  videoUrl = signal('');
   subtitles = [
     { label: 'part1', detail: 'Iag3UTqJ#zUMa0uCvhQLDk3Ean18oys2yClHgQ9tkcjvg5K4myPU' },
     { label: 'part2', detail: '9bZmzaRT#IQbIAunaqsO9SzytNj1ZhjeMjL7wOYA5SQM3N88y_84' },
@@ -44,18 +32,42 @@ export class AppComponent {
     { label: 'part4', detail: 'gDwQxDob#LrNr3O4tAqn9fKn0Jcod49JP-mOeRqrbavlqIxE6pao' },
     { label: 'part5', detail: 'wCoFTa7L#Jwjxt7LmrOBKapH9rpdi8u0LpkJRYWQCad_IzodzaLA' },
   ];
-  toggleExpand() {
-    this.expanded = !this.expanded;
+  books = [
+    {
+      title: 'Atomic Habits by James Clear',
+      subtitle: '5 Parts',
+      expanded: false,
+      parts: [
+        { label: 'part1', detail: 'Iag3UTqJ#zUMa0uCvhQLDk3Ean18oys2yClHgQ9tkcjvg5K4myPU' },
+        { label: 'part2', detail: '9bZmzaRT#IQbIAunaqsO9SzytNj1ZhjeMjL7wOYA5SQM3N88y_84' },
+        { label: 'part3', detail: '9fZxHA4R#CX_5z1CpEpYnHwPJYN_zEIri3SrQPa_VR_XkmpSUbsw' },
+        { label: 'part4', detail: 'gDwQxDob#LrNr3O4tAqn9fKn0Jcod49JP-mOeRqrbavlqIxE6pao' },
+        { label: 'part5', detail: 'wCoFTa7L#Jwjxt7LmrOBKapH9rpdi8u0LpkJRYWQCad_IzodzaLA' },
+      ],
+    },
+    {
+      title: 'Emotional Intelligence Why It Can Matter More Than IQ by Daniel Goleman',
+      subtitle: '3 Parts',
+      expanded: false,
+      parts: [
+        { label: 'Part 1', detail: 'https://mega.nz/file/AfhThSwB#tk3w_JF6yDesQRRljPx7Wz2y3sj-smhEw4VF27-FsTo' },
+        { label: 'Part 2', detail: 'https://mega.nz/file/8T4wHK5L#66j8T4TNfAersttMKeao94qFWmV1L20aKTdGYnZ8l88' },
+        { label: 'Part 3', detail: 'https://mega.nz/file/UWwx1CyA#UyCJ49GTB2Br9KVvJGmafMvNGgPOjZcU944gsuDS7jg' },
+      ],
+    },
+  ];
+  toggleExpand(book: any) {
+    book.expanded = !book.expanded;
   }
 
-  onSubClick(sub: any, index: number) {
-    console.log(`Clicked #${index}: ${sub.detail}`);
-    // toggle same item off if clicked again
-    this.activeIndex = this.activeIndex === index ? null : index;
-    this.loadAudio(sub.detail)
+  onSubClick(bookIndex: number, partIndex: number, part: any) {
+    console.log(`Book #${bookIndex + 1}, Part #${partIndex + 1}: ${part.detail}`);
+    this.activeIndex[bookIndex] =
+      this.activeIndex[bookIndex] === partIndex ? null : partIndex;
+    this.loadAudio(part.detail)
   }
   loadAudio(megastr:string){
     const megaFileUrl = encodeURIComponent(`https://mega.nz/file/${megastr}`);
-    this.videoUrl = `${environment.keyobUrl}mega/stream?url=${megaFileUrl}`;
+    this.videoUrl.set(`${environment.keyobUrl}mega/stream?url=${megaFileUrl}`)
   }
 }
