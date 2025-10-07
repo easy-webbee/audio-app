cf1eed935b77da53ffc608eb2dbc1a3e28354517
loc <lluu3790@gmail.com>
2025-10-07 16:09:04 -0500

    sss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule, } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { environment } from '../environments/environment';
 import { signal } from '@angular/core';
 import { dataAudio } from './data';
@@ -9,26 +9,29 @@ import { FormsModule } from '@angular/forms';
 import { ReplaceUSPipe } from './replace-us.pipe';
 import { Title } from '@angular/platform-browser';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TypeaheadModule, FormsModule, ReplaceUSPipe],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    TypeaheadModule,
+    FormsModule,
+    ReplaceUSPipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'audio-app';
-  audioTitles:any;
+  audioTitles: any;
   selected: any;
-  constructor(     private titleService: Title, ) {}
+  constructor(private titleService: Title) {}
 
   @ViewChild('audio', { static: true }) audioRef!: ElementRef<HTMLAudioElement>;
 
   ngOnInit() {
-    console.log(Object.keys(dataAudio))
-    this.audioTitles = Object.keys(dataAudio)
-    console.log(Object.values(dataAudio))
+    this.audioTitles = Object.keys(dataAudio);
     // const megaFileUrl = encodeURIComponent(
     //   'https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs'
     // );
@@ -41,12 +44,12 @@ export class AppComponent {
   // }
   activeIndex: { [bookIndex: number]: number | null } = {};
   videoUrl = signal('');
-  videoUrl2 :any;
+  videoUrl2: any;
   books = Object.values(dataAudio);
   toggleExpand(book: any) {
     book.expanded = !book.expanded;
   }
-  subtitles:any
+  subtitles: any;
   // onSubClick(bookIndex: number, partIndex: number, part: any) {
   //   console.log(`Book #${bookIndex + 1}, Part #${partIndex + 1}: ${part.detail}`);
   //   this.activeIndex[bookIndex] =
@@ -54,92 +57,96 @@ export class AppComponent {
   //   console.log(part)
   //   this.loadAudio(part.detail)
   // }
-  loadAudio(megastr:string){
+  loadAudio(megastr: string) {
     const megaFileUrl = encodeURIComponent(`${megastr}`);
-    this.videoUrl.set(`${environment.keyobUrl}stream/audio?url=${megaFileUrl}`)
+    this.videoUrl.set(`${environment.keyobUrl}stream/audio?url=${megaFileUrl}`);
   }
 
   onInputChange(input: any) {
-    console.log(input.target.value)
+    console.log(input.target.value);
   }
 
   onSearchSelected(input: any) {
-    this.subtitles = []
+    this.subtitles = [];
     this.filterTitles(input.value);
   }
   filterTitles(input: string) {
-    this.titleService.setTitle(this.selected.replace(/-/g, ''))
-    this.show = false
-    this.videoUrl.set('')
-    this.activeIndex ={}
+    this.titleService.setTitle(this.selected.replace(/-/g, ''));
+    this.show = false;
+    this.videoUrl.set('');
+    this.activeIndex = {};
     const filteredTitles: { [key: string]: any } = {};
     Object.keys(dataAudio).forEach((title) => {
-      console.log(title)
-      if (title.replace(/-/g, '').toLowerCase().includes(input.replace(/ /g, '').toLowerCase())) {
+      console.log(title);
+      if (
+        title
+          .replace(/-/g, '')
+          .toLowerCase()
+          .includes(input.replace(/ /g, '').toLowerCase())
+      ) {
         filteredTitles[title] = dataAudio[title];
       }
     });
-    console.log(filteredTitles)
-    this.subtitles = Object.values(filteredTitles)
-    console.log(Object.values(filteredTitles));
+    this.subtitles = Object.values(filteredTitles);
   }
-  clearInput(){
-    this.selected =''
-    this.show = true
-    this.subtitles = []
+  clearInput() {
+    this.selected = '';
+    this.show = true;
+    this.subtitles = [];
   }
-  show:boolean = true
-  getBook(book:string){
-    this.selected = book
+  show: boolean = true;
+  getBook(book: string) {
+    this.selected = book;
     this.filterTitles(book);
   }
   get displayAudioTitles() {
     return this.audioTitles.map((t: string) => t.replace(/_/g, ' '));
   }
   currentBookIndex: number | null = null;
-currentPartIndex: number | null = null;
+  currentPartIndex: number | null = null;
 
-onSubClick(bookIndex: number, partIndex: number, part: any) {
-  this.currentBookIndex = bookIndex;
-  this.currentPartIndex = partIndex;
-  this.activeIndex[bookIndex] = partIndex;
-  this.loadAudio(part.detail);
-}
-
-// Auto next when audio ends
-onAudioEnded(bookIndex: number, partIndex: number) {
-  const book = this.subtitles[bookIndex];
-  if (!book || !book.parts) return;
-
-  const nextIndex = partIndex + 1;
-  if (nextIndex < book.parts.length) {
-    const nextPart = book.parts[nextIndex];
-    this.onSubClick(bookIndex, nextIndex, nextPart);
-  } else {
-    console.log('End of this book reached!');
+  onSubClick(bookIndex: number, partIndex: number, part: any) {
+    this.currentBookIndex = bookIndex;
+    this.currentPartIndex = partIndex;
+    this.activeIndex[bookIndex] = partIndex;
+    this.loadAudio(part.detail);
   }
-}
 
-// Manual next
-playNext(bookIndex: number) {
-  if (this.currentBookIndex === null || this.currentPartIndex === null) return;
-  const book = this.subtitles[this.currentBookIndex];
-  const nextIndex = this.currentPartIndex + 1;
-  if (book && nextIndex < book.parts.length) {
-    const nextPart = book.parts[nextIndex];
-    this.onSubClick(this.currentBookIndex, nextIndex, nextPart);
+  // Auto next when audio ends
+  onAudioEnded(bookIndex: number, partIndex: number) {
+    const book = this.subtitles[bookIndex];
+    if (!book || !book.parts) return;
+
+    const nextIndex = partIndex + 1;
+    if (nextIndex < book.parts.length) {
+      const nextPart = book.parts[nextIndex];
+      this.onSubClick(bookIndex, nextIndex, nextPart);
+    } else {
+      console.log('End of this book reached!');
+    }
   }
-}
 
-// Manual previous
-playPrev(bookIndex: number) {
-  if (this.currentBookIndex === null || this.currentPartIndex === null) return;
-  const book = this.subtitles[this.currentBookIndex];
-  const prevIndex = this.currentPartIndex - 1;
-  if (book && prevIndex >= 0) {
-    const prevPart = book.parts[prevIndex];
-    this.onSubClick(this.currentBookIndex, prevIndex, prevPart);
+  // Manual next
+  playNext(bookIndex: number) {
+    if (this.currentBookIndex === null || this.currentPartIndex === null)
+      return;
+    const book = this.subtitles[this.currentBookIndex];
+    const nextIndex = this.currentPartIndex + 1;
+    if (book && nextIndex < book.parts.length) {
+      const nextPart = book.parts[nextIndex];
+      this.onSubClick(this.currentBookIndex, nextIndex, nextPart);
+    }
   }
-}
 
+  // Manual previous
+  playPrev(bookIndex: number) {
+    if (this.currentBookIndex === null || this.currentPartIndex === null)
+      return;
+    const book = this.subtitles[this.currentBookIndex];
+    const prevIndex = this.currentPartIndex - 1;
+    if (book && prevIndex >= 0) {
+      const prevPart = book.parts[prevIndex];
+      this.onSubClick(this.currentBookIndex, prevIndex, prevPart);
+    }
+  }
 }
