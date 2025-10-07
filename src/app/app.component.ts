bbcf8d959b44b3a035d4a0f46114a467e8d3ea21
loc <lluu3790@gmail.com>
2025-10-06 20:18:49 -0500

    test me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { environment } from '../environments/environment.development';
 import { signal } from '@angular/core';
+// import {dataAudio} from './data'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,10 +18,10 @@ export class AppComponent {
   @ViewChild('audio', { static: true }) audioRef!: ElementRef<HTMLAudioElement>;
 
   ngOnInit() {
-    const megaFileUrl = encodeURIComponent(
-      'https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs'
-    );
-    this.videoUrl2 = `${environment.keyobUrl}mega/stream?url=${megaFileUrl}`;
+    // const megaFileUrl = encodeURIComponent(
+    //   'https://mega.nz/file/oTgiCKib#M2TyXr8cBTJzUI1oFyyZ9L92Of0hRv5VxbDvOBncGbs'
+    // );
+    // this.videoUrl2 = `${environment.keyobUrl}mega/stream?url=${megaFileUrl}`;
   }
 
   // playAudio() {
@@ -30,13 +31,6 @@ export class AppComponent {
   activeIndex: { [bookIndex: number]: number | null } = {};
   videoUrl = signal('');
   videoUrl2 :any;
-  subtitles = [
-    { label: 'part1', detail: 'Iag3UTqJ#zUMa0uCvhQLDk3Ean18oys2yClHgQ9tkcjvg5K4myPU' },
-    { label: 'part2', detail: '9bZmzaRT#IQbIAunaqsO9SzytNj1ZhjeMjL7wOYA5SQM3N88y_84' },
-    { label: 'part3', detail: '9fZxHA4R#CX_5z1CpEpYnHwPJYN_zEIri3SrQPa_VR_XkmpSUbsw' },
-    { label: 'part4', detail: 'gDwQxDob#LrNr3O4tAqn9fKn0Jcod49JP-mOeRqrbavlqIxE6pao' },
-    { label: 'part5', detail: 'wCoFTa7L#Jwjxt7LmrOBKapH9rpdi8u0LpkJRYWQCad_IzodzaLA' },
-  ];
   books = [
     {
       title: 'Atomic Habits by James Clear',
@@ -55,9 +49,9 @@ export class AppComponent {
       subtitle: '3 Parts',
       expanded: false,
       parts: [
-        { label: 'Part 1', detail: 'https://mega.nz/file/AfhThSwB#tk3w_JF6yDesQRRljPx7Wz2y3sj-smhEw4VF27-FsTo' },
-        { label: 'Part 2', detail: 'https://mega.nz/file/8T4wHK5L#66j8T4TNfAersttMKeao94qFWmV1L20aKTdGYnZ8l88' },
-        { label: 'Part 3', detail: 'https://mega.nz/file/UWwx1CyA#UyCJ49GTB2Br9KVvJGmafMvNGgPOjZcU944gsuDS7jg' },
+        { label: 'Part 1', detail: 'AfhThSwB#tk3w_JF6yDesQRRljPx7Wz2y3sj-smhEw4VF27-FsTo' },
+        { label: 'Part 2', detail: '8T4wHK5L#66j8T4TNfAersttMKeao94qFWmV1L20aKTdGYnZ8l88' },
+        { label: 'Part 3', detail: 'UWwx1CyA#UyCJ49GTB2Br9KVvJGmafMvNGgPOjZcU944gsuDS7jg' },
       ],
     },
   ];
