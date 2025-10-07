697b7c9442839bcc2ae20b3277d47227d726acd2
loc <lluu3790@gmail.com>
2025-10-07 12:50:30 -0500

    aaa

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,29 +3,26 @@ export const dataAudio: Record<string, any> = {
     title:
       'Emotional Intelligence Why It Can Matter More Than IQ by Daniel Goleman',
     subtitle: '3 Parts',
-    expanded: true,
+    expanded: false,
     parts: [
       {
         label: 'Part 1',
-        detail:
-          'AfhThSwB#tk3w_JF6yDesQRRljPx7Wz2y3sj-smhEw4VF27-FsTo',
+        detail: 'AfhThSwB#tk3w_JF6yDesQRRljPx7Wz2y3sj-smhEw4VF27-FsTo',
       },
       {
         label: 'Part 2',
-        detail:
-          '8T4wHK5L#66j8T4TNfAersttMKeao94qFWmV1L20aKTdGYnZ8l88',
+        detail: '8T4wHK5L#66j8T4TNfAersttMKeao94qFWmV1L20aKTdGYnZ8l88',
       },
       {
         label: 'Part 3',
-        detail:
-          'UWwx1CyA#UyCJ49GTB2Br9KVvJGmafMvNGgPOjZcU944gsuDS7jg',
+        detail: 'UWwx1CyA#UyCJ49GTB2Br9KVvJGmafMvNGgPOjZcU944gsuDS7jg',
       },
     ],
   },
   Atomic_Habits_by_James_Clear: {
     title: 'Atomic Habits by James Clear',
     subtitle: '5 Parts',
-    expanded: true,
+    expanded: false,
     parts: [
       {
         label: 'part1',
@@ -52,7 +49,7 @@ export const dataAudio: Record<string, any> = {
   Jim_Rohn_Best_Life_Ever_Training: {
     title: 'Jim Rohn Best Life Ever Training',
     subtitle: '5 Parts',
-    expanded: true,
+    expanded: false,
     parts: [
       {
         label: 'part 1',
@@ -73,6 +70,123 @@ export const dataAudio: Record<string, any> = {
       {
         label: 'part 5',
         detail: 'ACRnDBxY#v2tS810U4W6Apma2Cp5A-TAfI7Kwkk9Wb8Il9Adworc',
+      },
+    ],
+  },
+  'The 10 Dollars Investor Growing Wealth on a shoestring budget Kenneth Scott':
+    {
+      title:
+        'The 10 Dollars Investor Growing Wealth on a shoestring budget Kenneth Scott',
+      subtitle: 'only',
+      expanded: false,
+      parts: [
+        {
+          label: 'part 1',
+          detail: '8KZhhKJZ#oKuh6_ACYkQoNhVmXu9FSMTlPJtadX3SX-nH0jN26CU',
+        },
+      ],
+    },
+  The_Gifts_of_Imperfection_by_Brene_Brown: {
+    title: 'The Gifts of Imperfection by Brene Brown',
+    subtitle: '3 Parts',
+    expanded: false,
+    parts: [
+      {
+        label: 'part 1',
+        detail: '9DxV0BrL#4RC7Teu6rRtKkhlNMVQD_9rrxFvGCVqArS7BVzKgXZc',
+      },
+      {
+        label: 'part 2',
+        detail: 'hX5zzRxC#ExTlaXR9fnlXk9x-yH-JXhouH6_aVQhJT1ZO71u4PH0',
+      },
+      {
+        label: 'part 3',
+        detail: 'pG4wGC6b#Cj52b47R7b9PgpUcvRWcwZYinxE34SRiZneV4ou17-8',
+      },
+    ],
+  },
+  '12_Rules_for_Life': {
+    title: '12 Rules for Life',
+    subtitle: '11 Parts',
+    expanded: false,
+    parts: [
+      {
+        label: 'part 1',
+        detail: 'FG5xlQrT#Ww885e0ZpVEOKn2_pRhYBcyvQ9lcrt5Ppa6PHGs5MQQ',
+      },
+      {
+        label: 'part 2',
+        detail: 'YWJmzQhZ#3EXQGvCmZgkHb41aTwMGOkmrDXDf-fqz7PKrDSvMSj0',
+      },
+      {
+        label: 'part 3',
+        detail: 'BSQlQZDQ#3ksbPrwuT8_XgYleWUG7UWEAVlmLLusqezbs96VM9QM',
+      },
+      {
+        label: 'part 4',
+        detail: '8CJA2Y4Z#dLG-nAbQ7M_Dtz9mddJ-LPml4vFTshu_ghkQuO92f3o',
+      },
+      {
+        label: 'part 5',
+        detail: 'oCRRhK5C#qVzCfAmLVrCN4nSyo02ScWgZa7lGVZJrC4pE1ar8uoM',
+      },
+      {
+        label: 'part 6',
+        detail: 'QKhlyRLI#VisQkw39CXGoPjTT3KLqyyhWjJzFudkZ1ALTtRBwvYY',
+      },
+      {
+        label: 'part 7',
+        detail: 'kHoCCDwB#WI78QQCl3l7MT8bYc2kmjO1MbRnTKP-CoxVJMtPtH2c',
+      },
+      {
+        label: 'part 8',
+        detail: 'IfxWBZpJ#I8bxgCGfENUVFIPNCyzPlDoTjdq5TMNo_rnJ164xqrs',
+      },
+      {
+        label: 'part 9',
+        detail: 'pTYWFQbT#mq7h30fvIjDleuKeRmqtA-2N5Yy8fAq2dvZEwbyKeqs',
+      },
+      {
+        label: 'part 10',
+        detail: 'UOZjhbJT#BWyoDOpTw30dzokdDGQ-QcYxNgFkRO4J-dJhhtKNx3I',
+      },
+      {
+        label: 'part 11',
+        detail: 'hW5CRJbT#D3EZ5L9otr7OSEdjZFSP7nCHLCuL12_PrixVTxgAjXU',
+      },
+    ],
+  },
+  '20_Life_Lessons': {
+    title: '20 Life Lessons',
+    subtitle: '3 Parts',
+    expanded: false,
+    parts: [
+      {
+        label: 'part 1',
+        detail: '4KQGxLLJ#8I7fv3x17tsTanMK15gd1DepOEBGTVEkLYLzB4KjcbE',
+      },
+      {
+        label: 'part 2',
+        detail: 'UaYGCJoA#YE4jB_8Yi9QGuuEPT4kqWqBvdTb5CjLOrN7Asa4Qr-w',
+      },
+      {
+        label: 'part 3',
+        detail: 'IOQhUILC#W9YCwtJyiYtHS5ViCIdkNUXB0IDbVT9OumTS2ns0Y1Y',
+      },
+    ],
+  },
+  '21_day_rule_by_Darren_Rowse': {
+    title: '21 day rule by Darren Rowse',
+    subtitle: '2 Parts',
+    expanded: false,
+    parts: [
+      {
+        label: 'part 1',
+        detail: '9LZRUQ5I#0J1xAMZQJn3WHKBVNNzhTLrrakKZVVyn3_oGB59FmUk',
+      },
+      {
+        label: 'part 2',
+        detail: 'hSBEmaIT#O6XRnFluDB7vj33eZ4Luxa7FIFQfxelxsvrRk46Kc9U',
       },
     ],
   },
