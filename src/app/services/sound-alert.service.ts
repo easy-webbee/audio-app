abce843291d70112e82a558352847f4e0a622c42
loc <lluu3790@gmail.com>
2026-09-12 17:17:57 -0500

    ....

--- src/app/services/sound-alert.service.ts
+++ src/app/services/sound-alert.service.ts
@@ -46,3 +46,60 @@ export class SoundAlertService {
   }
 }
  */
+
+
+
+
+
+
+/**
+ 
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SoundAlertService {
+
+//   playSound(filePath: string): void {
+//     const audio = new Audio(filePath);
+
+//     audio.volume = 0.5;
+
+//     audio.play().catch(err => {
+//       console.warn('Unable to play sound:', err);
+//     });
+//   }
+
+//   playAlert(soundName: string = 'bullish.mp3'): void {
+//     this.playSound(`./assets/${soundName}`);
+//   }
+// }
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SoundAlertService {
+
+  private audio?: HTMLAudioElement;
+
+  playSound(filePath: string): void {
+
+    this.audio?.pause();
+
+    this.audio = new Audio(filePath);
+    this.audio.volume = 0.5;
+
+    this.audio.play().catch(err => {
+      console.warn('Unable to play sound:', err);
+    });
+  }
+
+  playAlert(soundName: string = 'bullish.mp3'): void {
+    this.playSound(`./assets/${soundName}`);
+  }
+}
+
+ */
